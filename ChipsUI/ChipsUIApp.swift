dcb56ce5a89bfa75beb232bd1076f6fbbf29531f
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-09-19 21:04:26 +0300

    Initial Commit

---
+++ ChipsUI/ChipsUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChipsUIApp.swift
+//  ChipsUI
+//
+//  Created by test on 9/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ChipsUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
